41e9b59d26bce3a5cc3f10978626ca1c91a6cf4e
ryanmdavis <ryan.m.davis5@gmail.com>
2015-03-31 11:24:07 -0700

    updated README.m

--- README.m
+++ README.m
@@ -1,1 +1,11 @@
 % README file for LuxtronControl
+% Author: Ryan M Davis
+%
+% Instructions for use:
+% 1) Copy the parent folder 'LuxtronControl' to your matlab folder and add
+% the folder to your path
+% 2) Open up the script 'Luxtron.m' and set the user inputs indicated under
+% the user inputs heading.
+% 3) Be sure that the correct com port is selected, and be sure that you
+% have selected a good folder to save the data in.
+% 4) Run the script.  To stop the measurement press 'q' key
